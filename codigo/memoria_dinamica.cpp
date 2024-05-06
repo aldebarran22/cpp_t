9ba51358c68e1f1334ce1c497c96747bc4b3dc1a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 20:20:46 +0200

    20.20

--- codigo/memoria_dinamica.cpp
+++ codigo/memoria_dinamica.cpp
@@ -7,13 +7,33 @@ Reservar un array dinamico, rellenar de numeros aleatorios, imprimir, y liberar
 #include <cstdlib>
 
 int main(){
+	int *ptr = 0;
 	
 	// Inicializar la semilla de numeros aleatorios:
 	std::srand(std::time(NULL));
 	
-	for (int i = 0 ; i < 30 ; i++){
-		std::cout << std::rand() << "  ";
+	// Leer de teclado la cantidad de numeros:
+	int n;
+	
+	std::cout << "Cuantos numeros queremos: ";
+	std::cin >> n;
+	
+	// Reservar memoria:
+	ptr = new int[n];
+	 
+	// Inicializar
+	for (int i = 0 ; i < n ; i++){
+		ptr[i] =  std::rand() % 100;
+	}
+	
+	// Imprimir
+	for (int i = 0 ; i < n ; i++){
+		std::cout << ptr[i] << "  ";
 	}
 	std::cout << std::endl;
+	
+	// Liberar:
+	delete [] ptr;
+	ptr = 0;
 	
 }
