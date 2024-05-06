baf1f96e3410610fda7c9604dbd4469bdbb23b89
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 18:17:23 +0200

    18.17

--- codigo/repaso.cpp
+++ codigo/repaso.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-// Tipos básicos: char, int, long, float, double
+// Tipos básicos: 
+// char 1 byte, 
+// short int 2 byte
+// int 4 bytes
+// long 4 bytes
+// float, 
+// double
 
 void tipoIntChar(){
 	char c = 'A';
@@ -16,11 +22,23 @@ void tipoIntChar(){
 	std::cout << "TABLA ASCII" << std::endl;
 	for (unsigned char i = 0 ; i < 256 ; i++){
 		std::cout << (int) i << " " <<  i << std::endl;
+		
+		if (i == 255) break;
 	}
 }
 
+void sizeVars(){
+		std::cout << "char: ocupa: " << sizeof(char) << " bytes" << std::endl;
+		std::cout << "short int: ocupa: " << sizeof(short int) << " bytes" << std::endl;
+		std::cout << "int: ocupa: " << sizeof(int) << " bytes" << std::endl;
+		std::cout << "long: ocupa: " << sizeof(long) << " bytes" << std::endl;
+		std::cout << "long long: ocupa: " << sizeof(long long) << " bytes" << std::endl;
+		std::cout << "float: ocupa: " << sizeof(float) << " bytes" << std::endl;
+		std::cout << "double: ocupa: " << sizeof(double) << " bytes" << std::endl;
+}
 
 int main(){
-	tipoIntChar();
+	//tipoIntChar();
+	sizeVars();
 	return 0;
 }
