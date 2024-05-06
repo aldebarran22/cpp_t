517f16e74de644c0d7f0d02eddf3a0ad544f0faa
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 18:28:01 +0200

    18.27

---
+++ codigo/arrays.cpp
@@ -0,0 +1,8 @@
+/*
+Repaso de arrays
+- Definición, acceso, modificación
+- Acceso a traves del puntero
+- Pasar por parametro un array 
+- Posibles errores al devolver un array
+- Arrays dinamicos.
+*/
