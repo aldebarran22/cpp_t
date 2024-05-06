486d13afd6f78a94d5b14218f11e285248eb5041
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 17:53:32 +0200

    17.53

--- codigo/test.cpp
+++ codigo/test.cpp
@@ -1,6 +1,25 @@
+
+// FICHEROS DE CABECERA
 #include <iostream>
 
+// FICHEROS DE CABECERAS PROPIOS
+//#include "funciones.h"
+
+// definicion de macros
+#define MENSAJE "hola que tal"
+
+// VARIABLES GLOBALES
+
+// DEFINICION DE PROTOTIPOS
+void imprimirMensaje();
+
+
 int main(){
-	std::cout << "ok" <<std::endl;
+	imprimirMensaje();
 	return 0;
+}
+
+// IMPLEMENTACION DE OTRAS FUNCIONES
+void imprimirMensaje(){
+	std::cout << MENSAJE <<std::endl;
 }
