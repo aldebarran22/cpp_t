f3410f7adc33c12988110792c2e312aa9afe0b33
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-08 18:24:11 +0200

    18.24

--- codigo/objetos/Persona.cpp
+++ codigo/objetos/Persona.cpp
@@ -6,6 +6,7 @@ Persona::Persona()
 
 Persona::Persona(std::string nombre, int edad, double altura)
 {
+	// this es obligatorio para deshacer la ambiguedad
 	this->nombre = nombre;
 	this->edad = edad;
 	this->altura = altura;
@@ -14,6 +15,7 @@ Persona::Persona(std::string nombre, int edad, double altura)
 
 std::string Persona::to_string()
 {
+	// Aquí no es obligatorio this:
 	return this->nombre + " " + std::to_string(this->edad) + " " + std::to_string(this->altura);
 }
 
--- codigo/objetos/main.cpp
+++ codigo/objetos/main.cpp
@@ -2,8 +2,23 @@
 
 #include "Persona.h"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void testPersona(){
+	Persona p; // Llama al constructor por defecto
+	Persona p2("Ana", 33, 1.77);
+	Persona *ptr = nullptr;
+	
+	std::cout << p2.to_string() <<  std::endl;
+	
+	// Crear un objeto dinamicamente:
+	ptr = new Persona("Alberto", 55, 1.78);
+	std::cout << ptr->to_string() <<  std::endl;
+	delete ptr;
+	
+	ptr = nullptr;
+}
 
 int main(int argc, char** argv) {
+	testPersona();
 	return 0;
 }
