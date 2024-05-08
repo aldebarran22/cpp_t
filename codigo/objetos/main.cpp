7a3e1d846cf58bc67eea0f505a481ff3ac04300f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-08 18:41:08 +0200

    18.41

--- codigo/objetos/main.cpp
+++ codigo/objetos/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
 #include "Persona.h"
+#include "Direccion.h"
 
 
 void testPersona(){
+	Direccion dir("Gran Via");
 	Persona p; // Llama al constructor por defecto
-	Persona p2("Ana", 33, 1.77);
+	Persona p2("Ana", 33, 1.77, dir);
 	Persona *ptr = nullptr;
 	
 	std::cout << p2.to_string() <<  std::endl;
 	
 	// Crear un objeto dinamicamente:
-	ptr = new Persona("Alberto", 55, 1.78);
+	ptr = new Persona("Alberto", 55, 1.78, Direccion("Callao", 45));
 	std::cout << ptr->to_string() <<  std::endl;
 	delete ptr;
 	
