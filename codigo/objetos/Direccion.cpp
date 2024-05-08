f6d2321a2141c9a82dc87d850d4cbc33c128f31e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-08 18:34:10 +0200

    18.34

---
+++ codigo/objetos/Direccion.cpp
@@ -0,0 +1,15 @@
+#include "Direccion.h"
+
+													// Lista de iniciadores.
+Direccion::Direccion(std::string calle_, int numero_):calle(calle_), numero(numero_)
+{
+}
+
+std::string Direccion::to_string()
+{
+	return calle + " " + std::to_string(numero);
+}
+
+Direccion::~Direccion()
+{
+}
---
+++ codigo/objetos/Direccion.h
@@ -0,0 +1,19 @@
+#ifndef DIRECCION_H
+#define DIRECCION_H
+
+#include <string>
+
+class Direccion
+{
+	// Att. privados
+	std::string calle;
+	int numero;
+	
+	public:
+		Direccion(std::string="", int=0);
+		std::string to_string();
+		~Direccion();
+	protected:
+};
+
+#endif
