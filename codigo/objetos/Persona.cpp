eef8adc9fa28d5a58948e94699224db7576d840e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-08 18:15:36 +0200

    18.15

--- codigo/objetos/Persona.cpp
+++ codigo/objetos/Persona.cpp
@@ -4,6 +4,19 @@ Persona::Persona()
 {
 }
 
+Persona::Persona(std::string nombre, int edad, double altura)
+{
+	this->nombre = nombre;
+	this->edad = edad;
+	this->altura = altura;
+}
+
+
+std::string Persona::to_string()
+{
+	return this->nombre + " " + std::to_string(this->edad) + " " + std::to_string(this->altura);
+}
+
 Persona::~Persona()
 {
 }
--- codigo/objetos/Persona.h
+++ codigo/objetos/Persona.h
@@ -1,10 +1,19 @@
 #ifndef PERSONA_H
 #define PERSONA_H
 
+#include <string>
+
 class Persona
 {
+	private:
+		std::string nombre;
+		int edad;
+		double altura;
+	
 	public:
 		Persona();
+		Persona(std::string, int, double);
+		std::string to_string();
 		~Persona();
 	protected:
 };
