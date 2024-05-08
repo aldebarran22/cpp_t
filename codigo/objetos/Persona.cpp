c5eae33e0227d38ddb3761645a60708e380bb1ff
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-08 18:37:53 +0200

    18.37

--- codigo/objetos/Persona.cpp
+++ codigo/objetos/Persona.cpp
@@ -4,19 +4,20 @@ Persona::Persona()
 {
 }
 
-Persona::Persona(std::string nombre, int edad, double altura)
+Persona::Persona(std::string nombre, int edad, double altura, Direccion dir)
 {
 	// this es obligatorio para deshacer la ambiguedad
 	this->nombre = nombre;
 	this->edad = edad;
 	this->altura = altura;
+	this->dir = dir;
 }
 
 
 std::string Persona::to_string()
 {
 	// Aquí no es obligatorio this:
-	return this->nombre + " " + std::to_string(this->edad) + " " + std::to_string(this->altura);
+	return this->nombre + " " + std::to_string(this->edad) + " " + std::to_string(this->altura) + " " + this->dir.to_string();
 }
 
 Persona::~Persona()
--- codigo/objetos/Persona.h
+++ codigo/objetos/Persona.h
@@ -3,16 +3,19 @@
 
 #include <string>
 
+#include "Direccion.h"
+
 class Persona
 {
 	private:
 		std::string nombre;
 		int edad;
 		double altura;
+		Direccion dir;
 	
 	public:
 		Persona();
-		Persona(std::string, int, double);
+		Persona(std::string, int, double, Direccion);
 		std::string to_string();
 		~Persona();
 	protected:
