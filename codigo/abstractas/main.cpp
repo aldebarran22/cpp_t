c375d675a79b6924e91dc923e53271ab410184d2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-10 17:27:26 +0200

    17.27

--- codigo/abstractas/main.cpp
+++ codigo/abstractas/main.cpp
@@ -1,4 +1,15 @@
+#include "Figura.h"
+#include "Figura2D.h"
+#include "Figura3D.h"
+
+#include "Triangulo.h"
+#include "Cubo.h"
+#include "Cuadrado.h"
+#include "Cilindro.h"
+
 #include <iostream>
+
+
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
