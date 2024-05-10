83916d92483d3de558023dda35d124100a7620e2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-10 18:22:30 +0200

    18.22

--- codigo/multiple/main.cpp
+++ codigo/multiple/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include "DateTime.h"
-
+#include "DateTime2.h"
 
 int main(int argc, char** argv) {
 	DateTime dt(10, 5, 2024,18, 7, 20);
 	std::cout << "DT: " << dt.toString() << std::endl;
+	
+	DateTime2 dt2(10, 5, 2024,18, 7, 20);
+	std::cout << "DT: " << dt2.toString() << std::endl;
 	return 0;
 }
