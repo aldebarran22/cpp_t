5a62d7418cd44b47d95a5ebfcb5aca0766113271
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-14 20:47:30 +0200

    20.47

--- codigo_OCT_2024/cajero.c
+++ codigo_OCT_2024/cajero.c
@@ -7,6 +7,7 @@ int main(){
 	int importe;
 	int i;
 	int correcto = 0;
+	int numBilletes;
 	
 	for (i = 0 ; i < 3 ; i++){
 		printf("teclear importe: ");
@@ -21,6 +22,24 @@ int main(){
 	
 	if (correcto == 1){
 		puts("El desglose de billetes es:");
+		
+		if (importe >= 50){
+			numBilletes = importe / 50;
+			importe %= 50;
+			printf("Billetes de 50: %d\n", numBilletes);
+		}
+		
+		if (importe >= 20){
+			numBilletes = importe / 20;
+			importe %= 20;
+			printf("Billetes de 20: %d\n", numBilletes);
+		}
+		
+		if (importe >= 10){
+			numBilletes = importe / 10;
+			importe %= 10;
+			printf("Billetes de 10: %d\n", numBilletes);
+		}
 		
 	} else {
 		puts("No dispone de mas intentos");
