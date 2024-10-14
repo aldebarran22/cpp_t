bfc6fc6d3476bd7900360f276ac97d9756278211
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-14 19:52:31 +0200

    19.52

--- codigo_OCT_2024/macros_variables.c
+++ codigo_OCT_2024/macros_variables.c
@@ -5,14 +5,14 @@
 #define IVA 0.21
 
 int main(){
-	double precio = 120.56;
+	double precio = 120.0;
 	double iva;
 	double total;
 	
 	iva = precio * IVA;
 	total = precio + iva;
 	
-	printf("precio = %f", precio);
+	printf("precio = %5.2f, iva = %.2f, total = %.2f", precio, iva, total);
 	return 0;
 	
 }
