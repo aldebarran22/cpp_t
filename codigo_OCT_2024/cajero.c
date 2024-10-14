4d064974e1b33d1fc2e6688bc548dac50d1d9287
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-14 20:55:11 +0200

    20.55

--- codigo_OCT_2024/cajero.c
+++ codigo_OCT_2024/cajero.c
@@ -9,45 +9,48 @@ int main(){
 	int correcto = 0;
 	int numBilletes;
 	
-	for (i = 0 ; i < 3 ; i++){
-		printf("teclear importe: ");
-		scanf("%d", &importe);
-		
-		if (importe % 10 != 0){
-			
-			if (i < 2)
-				puts("Importe incorrecto, teclear un multiplo de 10");
-			else
-				puts("No dispone de mas intentos");
-					
-		} else {
-			correcto = 1;
-			break;
-		}			
-	}
+	while (1){
 	
-	if (correcto == 1){
-		puts("El desglose de billetes es:");
-		
-		if (importe >= 50){
-			numBilletes = importe / 50;
-			importe %= 50;
-			printf("Billetes de 50: %d\n", numBilletes);
+		for (i = 0 ; i < 3 ; i++){
+			printf("teclear importe: ");
+			scanf("%d", &importe);
+			
+			if (importe % 10 != 0){
+				
+				if (i < 2)
+					puts("Importe incorrecto, teclear un multiplo de 10");
+				else
+					puts("No dispone de mas intentos");
+						
+			} else {
+				correcto = 1;
+				break;
+			}			
 		}
 		
-		if (importe >= 20){
-			numBilletes = importe / 20;
-			importe %= 20;
-			printf("Billetes de 20: %d\n", numBilletes);
+		if (correcto == 1){
+			puts("El desglose de billetes es:");
+			
+			if (importe >= 50){
+				numBilletes = importe / 50;
+				importe %= 50;
+				printf("Billetes de 50: %d\n", numBilletes);
+			}
+			
+			if (importe >= 20){
+				numBilletes = importe / 20;
+				importe %= 20;
+				printf("Billetes de 20: %d\n", numBilletes);
+			}
+			
+			if (importe >= 10){
+				numBilletes = importe / 10;
+				importe %= 10;
+				printf("Billetes de 10: %d\n", numBilletes);
+			}
+			
 		}
-		
-		if (importe >= 10){
-			numBilletes = importe / 10;
-			importe %= 10;
-			printf("Billetes de 10: %d\n", numBilletes);
-		}
-		
-	} 
+	}
 		
 	return 0;
 }
