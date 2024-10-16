868d2f5f87df564124df64b89b1aab0fe55fd1c5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-16 18:29:51 +0200

    18.29

---
+++ codigo_OCT_2024/punteros.c
@@ -0,0 +1,15 @@
+// Punteros
+
+#include <stdio.h>
+
+int main(){
+	int numero = 100;
+	int *ptr = NULL;
+	
+	// Extraer la dir. de memoria de una variable
+	ptr = &numero;
+	*ptr = *ptr * 10; // Acceso al numero a traves del puntero.
+	printf("ptr = %p, *ptr = %d, numero = %d\n", ptr, *ptr, numero);
+	
+	return 0;
+}
