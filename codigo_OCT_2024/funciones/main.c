c0ac7441a44d3323b62461eea3aa670cb6cdf01b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-16 19:43:32 +0200

    19.43

--- codigo_OCT_2024/funciones/main.c
+++ codigo_OCT_2024/funciones/main.c
@@ -4,6 +4,8 @@
 #include "funciones.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+// atof -> array to float
+// atoi -> array to int
 
 int main(int argc, char *argv[]) {
 	double num1, num2;
