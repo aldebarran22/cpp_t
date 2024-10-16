8ea639984f5cfd8c79c97997cdd89dc255d51377
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-16 20:53:13 +0200

    20.52

---
+++ codigo_OCT_2024/funciones_parametros.c
@@ -0,0 +1,26 @@
+// Paso de parámetros por copia y por puntero
+
+#include <stdio.h>
+
+void pasoPorCopia(int numero){
+	numero++;
+	printf("n en paso PorCopia: %d\n",numero);
+}
+
+void pasoPorPtr(int *ptr){
+	*ptr = 200;
+}
+
+
+int main(){
+	int numero = 100;
+	
+	printf("numero: %d\n", numero);
+	pasoPorCopia(numero);
+	printf("numero: %d\n", numero);
+	
+	pasoPorPtr(&numero);
+	printf("numero: %d\n", numero);
+	
+	return 0;
+}
