6842d15d61cd7d1cda22618a93261a0a78942df5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-16 19:41:07 +0200

    19.40

--- codigo_OCT_2024/funciones/main.c
+++ codigo_OCT_2024/funciones/main.c
@@ -6,7 +6,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("suma: %g\n", sumar(12,34));
-	printf("resta: %g\n", restar(12,34));
+	double num1, num2;
+	
+	printf("Dame numero1: ");
+	scanf("%lf", &num1);
+
+	printf("Dame numero2: ");
+	scanf("%lf", &num2);
+	
+	printf("suma: %lf\n", sumar(num1,num2));
+	printf("resta: %lf\n", restar(num1,num2));
 	return 0;
 }
