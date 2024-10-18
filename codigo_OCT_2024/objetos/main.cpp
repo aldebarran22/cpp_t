31f7e147ec8054778820eb8bf195527b5f1cb27a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-18 18:57:10 +0200

    18.57

--- codigo_OCT_2024/objetos/main.cpp
+++ codigo_OCT_2024/objetos/main.cpp
@@ -7,12 +7,16 @@
 int main(int argc, char** argv) {
 	Persona p1("Pedro", 77, 1.8);
 	
+	// Uso del constructor copia:
+	Persona p2(p1);
+	
 	p1.engordar();
 	
 	std::cout << "Pruebas clase Persona: " << std::endl;
-	std::cout << p1.to_string() << std::endl;
+	std::cout << "p1: " << p1.to_string() << std::endl;
 	
 	p1.engordar(2.5);
-	std::cout << p1.to_string() << std::endl;
+	std::cout << "p1: " << p1.to_string() << std::endl;
+	std::cout << "p2: " << p2.to_string() << std::endl;
 	return 0;
 }
