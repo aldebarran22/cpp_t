8b5adbed4c3232333719ec5258670f8fc1164570
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-18 20:06:27 +0200

    20.06

--- codigo_OCT_2024/puntos/main.cpp
+++ codigo_OCT_2024/puntos/main.cpp
@@ -9,8 +9,13 @@ int main(int argc, char** argv) {
 	Punto2D q(-1, 7);
 	
 	std::cout << "p: " <<  p.to_string() << " q: "  << q.to_string() << std::endl;
+	
 	q.desplazar(2);
+	std::cout << "p: " <<  p.to_string() << " q.desplazar(2): "  << q.to_string() << std::endl;
+	
 	p = q.desplazarCopia(3);
+	
+	std::cout << "p = q.desplazarCopia(3): " <<  p.to_string() << " q: "  << q.to_string() << std::endl;
 	
 	
 	return 0;
