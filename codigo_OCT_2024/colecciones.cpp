33544f60a88a06217165df71d2c81450dc19310a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-21 20:46:12 +0200

    20.46

--- codigo_OCT_2024/colecciones.cpp
+++ codigo_OCT_2024/colecciones.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 
 int main(){
@@ -15,6 +16,8 @@ int main(){
 	std::vector<int>::reverse_iterator it2;
 	
 	std::vector<std::string> nombres = {"Pedro", "Miguel","Sofia","Ana"};
+	std::vector<std::string>::iterator itcad;
+	std::string buscar;
 	
 	// Añadir elementos al vector:
 	numeros.push_back(12);
@@ -54,6 +57,16 @@ int main(){
 		std::cout << *it2 << " ";
 	}
 	std::cout << std::endl;
+	
+	// Buscar un nombre dentro de la coleccion de nombres:
+	buscar = "Sofia";
+	itcad = std::find(nombres.begin(), nombres.end(), buscar); 
+	if (itcad == nombres.end()){
+		std::cout << "No existe " << buscar << std::endl;
+		
+	} else {
+		std::cout << "Existe " << *itcad << " posicion: " << itcad - nombres.begin() << std::endl;
+	}
 	
 	//std::cout << nombres[20] << std::endl;
 	//std::cout << nombres.at(20) << std::endl;
