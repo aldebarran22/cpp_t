12647ca55e030a08ce9df47bd055bb6ae13e588b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-23 20:15:48 +0200

    20.15

--- codigo_OCT_2024/figuras/Model/main.cpp
+++ codigo_OCT_2024/figuras/Model/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <typeinfo>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include "Figura.h"
+#include "Figura2D.h"
+#include "Figura3D.h"
+
+#include "Triangulo.h"
+#include "Cubo.h"
+#include "Cuadrado.h"
+#include "Cilindro.h"
+
 
 int main(int argc, char** argv) {
+	Figura *f;
+	Figura *array[4];
+	
+	f = new Cuadrado(10.0);
+	Figura2D *f2d = static_cast<Figura2D *>(f);
+	std::cout << "Area: " <<  f2d->area() << std::endl;
+	
+	array[0] = new Cuadrado(10.0);
+	array[1] = new Triangulo(4.6, 7.0);
+	array[2] = new Cubo(5.0);
+	array[3] = new Cilindro(5.6, 2.0);
+	
+	for (int i = 0 ; i < 4 ; i++){
+		array[i]->visualizar();
+		
+		delete array[i];
+		array[i] = nullptr;
+	}
+	
 	return 0;
 }
