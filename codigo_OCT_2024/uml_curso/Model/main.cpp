a46d75ca81ff211e368e09979a7ca2dc91a672b6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-23 19:14:24 +0200

    19.14

--- codigo_OCT_2024/uml_curso/Model/main.cpp
+++ codigo_OCT_2024/uml_curso/Model/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 #include "Empleado.h"
 #include "Administrativo.h"
@@ -9,10 +10,19 @@
 int main(int argc, char** argv) {
 	
 	Empleado prog1("Programador1", 70.0, 1.77, "CAS", 1800.0);
+	Empleado prog2("Programador2", 76.0, 1.87, "CAS", 1800.0);
+	Empleado prog3("Programador3", 79.0, 1.68, "CAS", 1800.0);
 	
+	Empleado jefe("Jefe", 90.0, 1.77, "CAS", 2300.0);
+	Administrativo adm("administrativo", 80.0, 1.8, "CAS", 1500.0, jefe);
 	
-	Empleado jefe("Tomas", 90.0, 1.77, "CAS", 2300.0);
-	Administrativo adm("Jose", 80.0, 1.8, "CAS", 1500.0, jefe);
+	JefeProyecto jp("Jefe de proyecto", 99.0, 1.9, "CAS",4500.0);
+	jp.addEmpleado(prog1);
+	jp.addEmpleado(prog2);
+	jp.addEmpleado(prog3);
+	jp.addEmpleado(adm);
+	
+	cout << jp.to_string() << endl;
 	
 	return 0;
 }
