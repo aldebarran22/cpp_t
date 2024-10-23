b31070ec267b3bd69adc0ca1973e9e62ba3d3115
aldebarran22 <antonioespinherranz@hotmail.com>
2024-10-23 19:11:06 +0200

    19.11

--- codigo_OCT_2024/uml_curso/Model/main.cpp
+++ codigo_OCT_2024/uml_curso/Model/main.cpp
@@ -7,5 +7,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	
+	Empleado prog1("Programador1", 70.0, 1.77, "CAS", 1800.0);
+	
+	
+	Empleado jefe("Tomas", 90.0, 1.77, "CAS", 2300.0);
+	Administrativo adm("Jose", 80.0, 1.8, "CAS", 1500.0, jefe);
+	
 	return 0;
 }
